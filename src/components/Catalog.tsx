21e133dc99db6ef8fab5371b814e11cf8f92f340
Kalleu Oliveira <xkalleu@gmail.com>
2021-04-18 21:26:41 -0300

    add: cart redux

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
-	const dispatch = useDispatch();
 	const [catalog, setCatalog] = useState<IProduct[]>([]);
 
 	useEffect(() => {
@@ -14,26 +12,11 @@ const Catalog: React.FC = () => {
 		});
 	}, []);
 
-	const handleAddProductToCart = useCallback(
-		(product: IProduct) => {
-			dispatch(addProductToCart(product));
-		},
-		[dispatch]
-	);
-
 	return (
 		<>
 			<h1>Catalog</h1>
 			{catalog.map((product) => (
-				<article key={product.id}>
-					<strong>{product.title}</strong>
-					{' - '}
-					<span>{product.price}</span>
-					{'  '}
-					<button onClick={() => handleAddProductToCart(product)} type="button">
-						Comprar
-					</button>
-				</article>
+				<CatalogItem key={product.id} product={product} />
 			))}
 		</>
 	);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,12 @@
+import { ICartState } from './modules/cart/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+export interface IState {
+	cart: ICartState;
+}
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
